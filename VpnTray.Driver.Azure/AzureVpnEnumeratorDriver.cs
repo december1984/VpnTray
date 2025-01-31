6a80730d41041a580e755e82669a06b28f65b852
Daniel Grudzień <Daniel.Grudzien@etteplan.com>
2025-01-31 13:30:47 +0100

    added support for azure vpn

---
+++ VpnTray.Driver.Azure/AzureVpnEnumeratorDriver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VpnTray.Domain;
+using VpnTray.Driver.WinApi;
+
+namespace VpnTray.Driver.Azure
+{
+    public class AzureVpnEnumeratorDriver : IVpnEnumeratorDriver
+    {
+        private string _phonebookPath;
+
+        public AzureVpnEnumeratorDriver(string phonebookPath)
+        {
+            _phonebookPath = phonebookPath;
+        }
+
+        public Task<Vpn[]> GetVpns()
+        {
+            return Task.FromResult((
+                    from name in RasApi32.RasEnumEntries(_phonebookPath)
+                    let entry = RasApi32.RasGetEntryProperties(name.EntryName, name.PhonebookPath)
+                    select new Vpn(entry.Id.ToString(), name.EntryName))
+                .ToArray());
+        }
+    }
+}
---
+++ VpnTray.Driver.Azure/AzureVpnProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using VpnTray.Domain;
+
+namespace VpnTray.Driver.Azure
+{
+    public class AzureVpnProvider : VpnProvider
+    {
+        private static string _phonebookPath = 
+            Path.Combine(Directory.GetDirectories(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Packages"), "Microsoft.AzureVpn*").FirstOrDefault() ?? string.Empty, 
+                "LocalState", "rasphone.pbk");
+
+        public AzureVpnProvider(ISystemEventsProvider systemEventsProvider)
+            : base(new AzureVpnEnumeratorDriver(_phonebookPath), new AzureVpnConnectorDriver(_phonebookPath), new VpnMonitorDriver(), systemEventsProvider)
+        {
+            Name = "Azure VPN";
+        }
+        public override string Name { get; protected set; }
+    }
+}
